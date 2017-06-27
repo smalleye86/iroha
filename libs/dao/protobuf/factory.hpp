c400001ed23c7136184d3c3473176fbab4206ee9
Bulat Nasrulin <bulat.nasrulin@gmail.com>
2017-06-27 10:41:45 +0300

    Serialization factory draft

---
+++ libs/dao/protobuf/factory.hpp
@@ -0,0 +1,48 @@
+/**
+ * Copyright Soramitsu Co., Ltd. 2017 All Rights Reserved.
+ * http://soramitsu.co.jp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include <block.pb.h>
+#include <dao/dao.hpp>
+/*
+ * Class for casting dao classes to protobuf
+ */
+#pragma once
+
+namespace iroha {
+class SerializeFactory {
+
+  // dao to command
+
+  virtual iroha::protocol::Block dao2proto(iroha::dao::Block) = 0;
+
+  virtual iroha::protocol::Transaction dao2proto(iroha::dao::Transaction) = 0;
+
+  virtual iroha::protocol::Command dao2proto(iroha::dao::Command) = 0;
+
+  // Command to dao
+
+  virtual iroha::dao::Command proto2dao(iroha::protocol::Command) = 0;
+
+  virtual iroha::dao::Transaction proto2dao(iroha::protocol::Transaction) = 0;
+
+  virtual iroha::dao::Block proto2dao(iroha::protocol::Block) = 0;
+
+};
+}
+
+
